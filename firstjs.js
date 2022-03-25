250cb446832b7c38428eb71040735caf6806b22e
tinysoccerball <91271377+tinysoccerball@users.noreply.github.com>
2022-03-25 10:14:02 -0400

    Update firstjs.js

--- firstjs.js
+++ firstjs.js
@@ -1,77 +1,130 @@
 let data = {
-     "featurePointAbbrv": "prn",
-     "MagnitudeOfChange": 2,
-     "NumberOfChangeX": 4,
-     "NumberOfChangeY": 3,
-     "NumberOfChangeZ": 2,
-     "InfluenceRadius": 20
+    "featurePointAbbrv": "prn",
+    "MagnitudeOfChange": 2,
+    "NumberOfChangeX": 4,
+    "NumberOfChangeY": 3,
+    "NumberOfChangeZ": 2,
+    "InfluenceRadius": 20
 }
+
+let counter = 1;
 
 var origdirectory = '';
 var basename = "JuanGlinton";
 
 var jsonText = {ModificationFiles:[{
-    threeDModel: "jack",
-    threeDModel: basename,
-    OriginalOBJFile: basename + ".obj",
-    OriginalJSONFile: basename + ".json",
-    TargetOBJFile: basename + "-Target-1.obj",
-    TargetJSONFile: basename + "-Target-1.json",
-    Folder: ".",
-    FallOffType: "",
-    modifications: [{
-        feature_abbrv: "ac_l",
-        deltaX: "10",
-        deltaY: "10",
-        deltaZ: "0",
-        InfluenceRadius: "20"
-                }]}]
-  };
-  var modelName = "hank";
-  var modelGender = "Male";
-  var modelAge = 20;
-
-  var test2 = {
-    threeDModel: modelName,
-    gender: modelGender,
-    age: modelAge,
-    //features: createFeatureText(),
-    //measurements: createMeasurementText()
-  };
-  var jsonReadyText = JSON.stringify(jsonText, null, 4);
+   threeDModel: "jack",
+   threeDModel: basename,
+   OriginalOBJFile: basename + ".obj",
+   OriginalJSONFile: basename + ".json",
+   TargetOBJFile: basename + "-Target-1.obj",
+   TargetJSONFile: basename + "-Target-1.json",
+   Folder: ".",
+   FallOffType: ""
+   //modifications: modlist()
+   }]
+ };
+ //var modelName = "hank";
+ var modelGender = "Male";
+ var modelAge = 20;
+/*
+ var test2 = {
+   threeDModel: modelName,
+   gender: modelGender,
+   age: modelAge,
+   //features: createFeatureText(),
+   //measurements: createMeasurementText()
+ };
+ var jsonReadyText = JSON.stringify(jsonText, null, 4);*/
 ////////////////////////////////////////////////
-    var dx = 0;
-    var dy = 0;
-    var dz = 0;
-    var changelist = [];
+   var dx = 0;
+   var dy = 0;
+   var dz = 0;
+   var changelist = [];
 
-    changes = data["NumberOfChangeX"];
+   changes = data["NumberOfChangeX"];
 if (changes <= data["NumberOfChangeY"]){
-    changes = data["NumberOfChangeY"]
+   changes = data["NumberOfChangeY"]
 }
 if (changes <= data["NumberOfChangeZ"]){
-    changes = data["NumberOfChangeZ"]
+   changes = data["NumberOfChangeZ"]
 }
 
 for (xmag = 0; xmag < changes; xmag++){
-    for (ymag = 0; ymag < changes; ymag++){
-        for (zmag = 0; zmag < changes; zmag++){
-            changelist.push([dx, dy, dz]);
-            if (dz < (data["NumberOfChangeZ"] * data["MagnitudeOfChange"])){
-                dz += data["MagnitudeOfChange"];
-            }
-            changelist.push([dx, dy, dz]);
-        }
-        if (dy < data["NumberOfChangeY"] * data["MagnitudeOfChange"]){
-            dy += data["MagnitudeOfChange"];
-        }
-        dz = 0;
-    }
-    if (dx < (data["NumberOfChangeX"] * data["MagnitudeOfChange"])){
-        dx += data["MagnitudeOfChange"];
-    }
-    dy = 0;
-    dz = 0;
+   for (ymag = 0; ymag < changes; ymag++){
+       for (zmag = 0; zmag < changes; zmag++){
+           changelist.push([dx, dy, dz]);
+           if (dz < (data["NumberOfChangeZ"] * data["MagnitudeOfChange"])){
+               dz += data["MagnitudeOfChange"];
+           }
+           changelist.push([dx, dy, dz]);
+       }
+       if (dy < data["NumberOfChangeY"] * data["MagnitudeOfChange"]){
+           dy += data["MagnitudeOfChange"];
+       }
+       dz = 0;
+   }
+   if (dx < (data["NumberOfChangeX"] * data["MagnitudeOfChange"])){
+       dx += data["MagnitudeOfChange"];
+   }
+   dy = 0;
+   dz = 0;
 }
 
-console.log(changelist);
+
+function makemods(point, dx, dy, dz, infl) { 
+   mod = {
+       "feature_abbrv": point,
+       "deltaX": dx,
+       "deltaY": dy,
+       "deltaZ": dz,
+       "InfluenceRadius": infl
+               }
+   //console.log(mod);
+   return mod;
+}
+
+function modlist() {
+   mods = [];
+   for(i = 0; i < (changelist.length); i++){
+       mods.push(makemods(data["featurePointAbbrv"], changelist[i][0], changelist[i][1], changelist[i][2], data["InfluenceRadius"]));
+   }
+   //console.log(mods);
+   return mods;
+}
+
+var modelName = "Kim";
+
+function makefile(){
+   file = {
+       "threeDModel": modelName,
+       "OriginalOBJFile": modelName + ".obj",
+       "OriginalJSONFile": modelName + ".json",
+       "TargetOBJFile": modelName + "-Target-" + counter + ".obj",
+       "TargetJSONFile": modelName + "-Target-" + counter +".json",
+       "Folder": ".",
+       "FallOffType": "",
+       "modifications": modlist()
+   }
+   return file
+}
+
+function myfiles(){
+   files = []
+   for (i = 0; i < 3; i++) {
+       files.push(makefile())
+       counter++;
+   }
+   return files;
+}
+
+function finalmodfiles(){
+   modfiles = {
+       "ModificationFiles":myfiles()
+   }
+   return modfiles;
+}
+
+var thejson = JSON.stringify(finalmodfiles(), null, 4);
+
+console.log(thejson);

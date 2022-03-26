4690c8af43b8ba20963f5e0076402ed78abca2df
tinysoccerball <91271377+tinysoccerball@users.noreply.github.com>
2022-03-26 17:55:50 -0400

    Update firstjs.js
    remove duplicate mods

--- firstjs.js
+++ firstjs.js
@@ -71,7 +71,31 @@ for (xmag = 0; xmag < changes; xmag++){
    dz = 0;
 }
 
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+  
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
+  
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
 
+function removeDuplicates(arr) {
+    for (i= 0; i<arr.length; i++){
+        if (arraysEqual(arr[i], arr[i+1])){
+            arr.splice(i, 1); 
+            i--; 
+        }
+    }
+    return arr;
+}
 function makemods(point, dx, dy, dz, infl) { 
    mod = {
        "feature_abbrv": point,
@@ -86,6 +110,7 @@ function makemods(point, dx, dy, dz, infl) {
 
 function modlist() {
    mods = [];
+   changelist = removeDuplicates(changelist);
    for(i = 0; i < (changelist.length); i++){
        mods.push(makemods(data["featurePointAbbrv"], changelist[i][0], changelist[i][1], changelist[i][2], data["InfluenceRadius"]));
    }

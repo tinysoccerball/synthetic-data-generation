2aa2316fb4f240c7a5ea35c2dfb2eed19ecaee75
tinysoccerball <91271377+tinysoccerball@users.noreply.github.com>
2022-04-04 15:56:08 -0400

    added table

--- instructionCreation.js
+++ instructionCreation.js
@@ -401,7 +401,33 @@ function addMod() {
     alert("Modification Added!");
     //console.log(inputs);
     modcount++;
-    document.getElementById("counter").innerHTML = "Added modifications: " + modcount;
+    var container = document.getElementById('container');
+    container.innerHTML = '';
+    // create table element
+    var table = document.createElement('table');
+    var tbody = document.createElement('tbody');
+    // loop array
+    for (i = 0; i < inputs.length; i++) {
+        // get inner array
+        var vals = inputs[i];
+        // create tr element
+        var row = document.createElement('tr');
+        // loop inner array
+        for (var b = 0; b < vals.length; b++) {
+            // create td element
+            var cell = document.createElement('td');
+            // set text
+            cell.textContent = vals[b];
+            // append td to tr
+            row.appendChild(cell);
+        }
+        //append tr to tbody
+        tbody.appendChild(row);
+    }
+    // append tbody to table
+    table.appendChild(tbody);
+    // append table to container
+    container.appendChild(table);
 }
 
 // Start file download.
